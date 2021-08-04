b63f5bc617af262bf05e3efad5b71c6871638310
Lee Seokhun <72291472+lsh9955@users.noreply.github.com>
2021-08-04 12:40:04 +0900

    Create 네트워크.js

---
+++ 이석훈/네트워크.js
@@ -0,0 +1,30 @@
+function solution(n, computers) {
+  let leftArr = Array(n)
+    .fill()
+    .map((v, i) => i);
+  let network = [[]];
+  let count = 0;
+  while (count <= n - 1) {
+    if (!isNaN(leftArr[count])) {
+      findroute(count);
+      network.push([]);
+      count = 0;
+    }
+    count += 1;
+  }
+
+  function findroute(num) {
+    leftArr[num] = "n";
+    if (network[network.length - 1] == "") {
+      network[network.length - 1].push(num);
+    }
+    for (let i = 0; i < n; i++) {
+      if (computers[num][i] === 1 && !network[network.length - 1].includes(i)) {
+        network[network.length - 1].push(i);
+        findroute(i);
+      }
+    }
+  }
+
+  return network.length - 1;
+}
