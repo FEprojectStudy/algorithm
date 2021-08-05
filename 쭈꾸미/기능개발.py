014d5b767ecb8080bd2ae976cf113c2cba557f62
Dalping <74459925+Bokyung626@users.noreply.github.com>
2021-08-06 06:35:44 +0900

    Update 기능개발.py

--- 쭈꾸미/기능개발.py
+++ 쭈꾸미/기능개발.py
@@ -6,16 +6,17 @@ def solution(progresses, speeds):
     answer = []
     count = 0
 
-    while p:
-        while p[0] >= 100:  # 배포
+    while p: #배포 가능여부 판단
+        while p[0] >= 100:
             p.popleft()
             s.popleft()
             count += 1
             if len(p) == 0: break
-        if count != 0:
+        if count != 0: #배포 가능한 작업이 있는 경우
             answer.append(count)
             count = 0
 
+        #작업 수행
         for i in range(len(p)):
             p[i] = p[i] + s[i]
 
