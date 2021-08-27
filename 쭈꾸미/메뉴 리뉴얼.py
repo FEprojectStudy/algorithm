9733e56a3bbd9aad82375c239c8118d209b062dd
Dalping <kbing14@naver.com>
2021-08-27 19:08:53 +0900

    Update 메뉴 리뉴얼.py

--- 쭈꾸미/메뉴 리뉴얼.py
+++ 쭈꾸미/메뉴 리뉴얼.py
@@ -2,12 +2,15 @@ from itertools import combinations
 
 def solution(orders, course):
     answer = []
+    tmp = []
     dict = {}
+    tmp_len = 0
+    max_val = 0
 
-    for idx, order in enumerate(orders):
+    for order in orders:
         arr = sorted(list(set(list(''.join(order)))))
         for i in course:
-            if(len(arr) < i): break; #조합 생성 불가
+            if len(arr) < i : break; #조합 생성 불가
 
             for j in list(combinations(arr, i)):
                 dict_key = ''.join(j)
@@ -16,14 +19,29 @@ def solution(orders, course):
                 else:
                     dict[dict_key] = dict[dict_key] + 1
 
-    key_arr = sorted(dict.keys(), reverse=True, key=lambda x: len(x))
+    key_arr = sorted(dict.keys(), key=lambda x: len(x))
+
     for k in key_arr:
+
+        if len(k) != tmp_len: #길이 맞추기
+            tmp_len = len(k)
+            for v in tmp:
+                answer.append(v)
+            tmp = []
+            max_val = 0
+
         if dict[k] >= 2:
-            answer.append(k)
+            if dict[k] > max_val:
+                tmp = [k]
+                max_val = dict[k]
+            elif dict[k] == max_val:
+                tmp.append(k)
+
+    for v in tmp:
+        answer.append(v)
 
     return sorted(answer)
-    # ['!AC', 'ACD', '!ACDE', 'ACE', 'AD', 'ADE', 'AE', 'BC', 'BCF', '!BCFG', 'BCG', 'BF', 'BFG', 'BG', 'CD', '!CDE', 'CE', 'CF', 'CFG', 'CG', 'DE', 'FG']
 
 order = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
 course = [2,3,4]
-print(solution(order,course))
+print(solution(order, course))
