a9714c7d7e098bc751fab8117d2c7cc218b2a72b
hiyoo95 <hiyoo95@gmail.com>
2021-10-10 09:52:44 +0900

    min_wallet size problem commit

---
+++ 알렉스데브/최소직사각형/b_card.py
@@ -0,0 +1,17 @@
+def solution(sizes):
+    answer = 0
+    cards = sizes
+    wallet_width = 0
+    wallet_height = 0
+
+    for i in range(len(cards)):
+      x = max(cards[i][0],cards[i][1])
+      y = min(cards[i][0],cards[i][1])
+      wallet_width = max(wallet_width, x)
+      wallet_height = max(wallet_height, y)
+   
+    answer = wallet_width * wallet_height
+
+    return answer
+
+print(solution([[60,50],[30,70],[60,30],[80,40]]))
